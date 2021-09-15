978d37efea5ab779705b5d10013d1498bc95b8e0
RyanWelford <ryan.welford@datastax.com>
2021-09-14 20:23:58 -0500

    init

---
+++ components/Card.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="card" :class="rarity">
+        <img class="owl" :src="imgURL" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    props: {
+        owl: {
+            type: Object,
+            default () {
+                return {
+                    rarity: 'none',
+                    igmURL: ''
+                }
+            }
+        }
+    },
+    data() {
+        return {
+            rarity: this.owl.rarity,
+            imgURL: this.owl.imgURL
+        }
+    }
+})
+</script>
+
+<style scoped>
+.card {
+    width: 100%;
+    border-radius: 12px;
+    box-shadow: #ebedf0 0 4px 12px;
+    position: relative;
+    padding-top: 140%
+}
+
+.card.rare {
+    background: #f1ba24;
+}
+.card.uncommon {
+    background: #b12659;
+}
+.card.common {
+    background: #72286d;
+}
+
+.owl {
+    width: 50%;
+    left: 50%;
+    top: 50%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+}
+</style>
+
+
